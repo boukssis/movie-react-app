3c315a0e7f9da8e42130beb8809a5d034f6bb16e
Boukssis_hicham <hichamboukssis@gmail.com>
2022-02-15 17:28:02 +0100

    Implement Routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import "./App.scss";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home";
+import MovieDetail from "./components/MovieDetail/MovieDetail";
+import Header from "./components/Header/Header";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
-      <h1>logo</h1>
+      <Router>
+        <Header/>
+        <Routes>
+        <Route path='/' element={<Home/>} />
+          <Route path="/movie/:imbdID" element={<MovieDetail/>} />
+          <Route path="*" element={<PageNotFound/>} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
-
+  
 export default App;
- 
