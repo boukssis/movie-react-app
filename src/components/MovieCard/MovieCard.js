66492ba7a23d53015a5a6f7dbf73e2db29a5d4cb
Boukssis_hicham <hichamboukssis@gmail.com>
2022-02-16 12:50:38 +0100

    Implement movie card and movie list + home page

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,8 +1,23 @@
 import React from 'react'
-
-function MovieCard() {
+import './MovieCard.scss'
+function MovieCard(props) {
+ const data = props.movie
+  console.log("datata",data);
   return (
-    <div>MovieCard</div>
+    <div className='card-item'> 
+   <div className="card-inner">
+     <div className="card-top">
+       <img src={data.Poster} alt={data.Title} />
+     </div>
+     <div className="card-bottom">
+       <div className="card-info">
+         <h4>{data.Title}</h4>
+         <p>{data.Year}</p>
+       </div>
+     </div>
+   </div>
+   
+      </div>
   )
 }
 
